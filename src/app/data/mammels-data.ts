4107b42af73da6a5c8bc654d92725b9f52dc87d3
Chathuni Sooriyaarachchi <chathuni.20230146@iit.ac.lk>
2026-04-19 23:08:47 +0530

    update pictures

--- src/app/data/mammels-data.ts
+++ src/app/data/mammels-data.ts
@@ -3,120 +3,140 @@ export const mammals = [
     id: 1,
     title: 'Mammals',
     description: 'Mammals, a class of vertebrates, are warm-blooded animals that nourish their young with milk. Mammals are adaptable to various weather conditions and being warm blooded, they can live in a wide variety of places. Mammals come in all shapes and sizes. There are around 5,500 species of mammals. The majority of mammals give birth to live young, but there are a few species that lay eggs. These are known as monotremes, and include the platypus.',
-    photo: '/mammals.jpg'
+    photo: '/mammals.jpg',
+    category:'MAMMELS'
   },
   {
     id: 2,
     title: 'Mammals are everywhere',
     description: 'Mammals are found all over the world. Polar bears are found in very cold places like the Arctic ocean, whereas Camels live in hot climates of Asia, Africa and Australia. Moles live under the ground and Bats in caves. Dolphins live in the oceans.',
-    photo: '/mammals-everywhere.jpg'
+    photo: '/fox.jpg',
+    category:'MAMMELS'
   },
   {
     id: 3,
     title: 'Mammals Characteristics',
     description: 'The main characteristics of mammals are warm blood, hairy bodies, a four-chambered heart, a single jaw bone, sweat glands and also mammary glands in females. Mammals have a layer of brain that gives them superior intelligence to reptiles and birds.',
-    photo: '/mammals-characteristics.jpg'
+    photo: '/polarbear.jpg',
+    category:'MAMMELS'
   },
   {
     id: 4,
     title: 'Small Mammals',
     description: 'Small mammals are varied and are found across categories. Common small mammals include rodents, bats, dogs, bears, cats, deer, sheep, goats, and humans. Some small mammals live in the sea and some on land. Flesh eating mammals that walk on their toes include cats, dogs, wolves, slender-tailed meerkats and lemurs. Flesh eating mammals that walk partly on their toes and partly on the soles of their feet include the skunk, weasel, otter and mink. Most small mammals measure less than a foot which makes them far more common than large mammals like elephants, tigers, etc.',
-    photo: '/small-mammals.jpg'
+    photo: '/dolphin.jpg',
+    category:'SMALLMAMMELS'
   },
   {
     id: 5,
     title: 'Did you know?',
     description: '1. The duck-billed platypus and the spiny anteater are mammals that lay eggs rather than give birth to live offspring. 2. The sloth, armadillo and anteater are part of a group called toothless mammals. They have no teeth in the front and therefore, appear toothless.',
-    photo: '/did-you-know.jpg'
+    photo: '/squirrel.jpg',
+    category:'MAMMELS'
   },
   {
     id: 6,
     title: 'Gnawing Mammals',
     description: 'The rat, mouse, squirrel, chipmunk, prairie dog, rabbit, hare and beaver are part of the group known as Gnawing mammals. All of them, except the hare and rabbit, have two very large, hard and sharp front teeth.',
-    photo: '/gnawing-mammals.jpg'
+    photo: '/rat.jpg',
+    category:'GNAWINGMAMMELS'
   },
   {
     id: 7,
     title: 'Flexibly Fingered Mammals',
     description: 'The flexibly fingered mammals are also known as primates. The monkey, lemur and even man are primates. They have flexible fingers and toes with nails. Primates are hairy, some have tails and can move either on two legs or four.',
-    photo: '/primates.jpg'
+    photo: '/lemur.jpeg',
+    category:'FLEXIBLYFINGEREDMAMMELS'
   },
   {
     id: 8,
     title: 'Insect Eating Mammals',
     description: 'The insect eating mammals include the mole, shrew and hedgehog. The mole is blind, eats grubs and worms and digs long burrows in the dirt. The hedgehog looks like a porcupine as it has spiky hair. It eats only insects. The shrew looks like the mole, but it also eats mice and other shrews.',
-    photo: '/insect-eaters.jpg'
+    photo: '/hedgehog.jpg',
+    category:'INSECTEATINGMAMMELS'
   },
   {
     id: 9,
     title: 'Rodents',
     description: 'Forty percent of the mammal species are rodents. The main physical characteristic of a rodent are its teeth. They have two pairs of constantly growing razor-sharp teeth. There are more than 1,700 species of rodents, found on all continents besides Antarctica, in large numbers. Common rodents include mice, rats, squirrels, porcupines, beavers, chipmunks, guinea pigs, and voles. Most rodents eat seeds or plants, but some rodent species have varied diets.',
-    photo: '/rodents.jpg'
+    photo: '/porcupine.jpg',
+    category:'INSECTEATINGMAMMELS'
   },
   {
     id: 10,
     title: 'Did you know?',
     description: 'Almost all the mammals live on land. Bats are the only mammals that can fly.',
-    photo: '/did-you-know-2.jpg'
+    photo: '/mole.jpg',
+    category:'MAMMELS'
   },
   {
     id: 11,
     title: 'Large Mammals',
     description: 'Mammals are of various sizes, but large mammals are indeed the biggest living things found anywhere in the world. The biggest mammals on land are the elephants followed by the rhinoceros and the hippopotamus. In water, it is the blue whale.',
-    photo: '/large-mammals.jpg'
+    photo: '/giraffe.jpg',
+    category:'LARGEMAMMELS'
   },
   {
     id: 12,
     title: 'Blue Whale',
     description: 'The world\'s biggest ever animal is a mammal, the Blue Whale. It can grow to a length of 33m (110ft) and weigh 190 tonnes.',
-    photo: '/blue-whale.jpg'
+    photo: '/blue-whale.jpg',
+    category:'LARGEMAMMELS'
   },
   {
     id: 13,
     title: 'Fact File: Blue Whale',
     description: 'A blue whale\'s tongue alone weighs as much as an elephant. About 100 people can fit in a blue whale\'s mouth. A blue whale\'s heart is the size of a Mini Cooper and can weigh close to 2,000 pounds (907 kg). Its heartbeat can be detected from two miles away and a human can easily crawl through its major arteries. Blue whales can produce sounds louder than a jet engine and can communicate with other whales 1,000 miles away.',
-    photo: '/blue-whale-facts.jpg'
+    photo: '/blue-whale-facts.jpg',
+    category:'MAMMELS'
   },
   {
     id: 14,
     title: 'Even-Toed Ungulate',
     description: 'The even-toed ungulates (hoofed animals) like the hippopotamus and giraffe are large mammals. The hippopotamus is the heaviest, while the giraffe is the tallest. Other even-toed ungulates are often overshadowed by the hippopotamus\' bulk and dwarfed by the giraffe\'s height. These include camels, pigs, deer, pronghorn, cattle, and antelope.',
-    photo: '/even-toed-ungulates.jpg'
+    photo: '/hippopotamus.jpg',
+    category:'LARGEMAMMELS'
   },
   {
     id: 15,
     title: 'Odd-Toed Ungulate',
     description: 'Odd-toed ungulates are a group of mammals that have either one or three toes. Horses, zebras, asses, tapirs, and rhinoceroses are examples of odd-toed ungulates. They are found in Africa, Asia and even the Americas and include domesticated horses and donkeys. The white rhinoceros and the hippopotamus rival each other as being the largest land animal after elephants. While all rhinos have horns, only the African species use them to fight with their rivals.',
-    photo: '/odd-toed-ungulates.jpg'
+    photo: '/rhinoceros.jpg',
+    category:'LARGEMAMMELS'
   },
   {
     id: 16,
     title: 'Pouched Mammals',
     description: 'Some mammals that have pouches in which they place their new born young are called pouched mammals. They are also known as marsupials. They include the kangaroo, koala, opossum and wallaby and are mostly found in Australia. The red kangaroo is the largest of the marsupials.',
-    photo: '/pouched-mammals.jpg'
+    photo: '/koala.jpg',
+    category:'LARGEMAMMELS'
   },
   {
     id: 17,
     title: 'Primate',
     description: 'Primates are also classified as \'Flexible Finger Mammals\'. This group consists of both, large and small mammals. Primates include lemurs, lorises, bush-babies, monkeys, apes and humans. The largest primate is the Gorilla. Most primates live in the tropics and subtropics.',
-    photo: '/gorilla.jpg'
+    photo: '/gorilla.jpg',
+    category:'LARGEMAMMELS'
   },
   {
     id: 18,
     title: 'Proboscidea',
     description: 'The African elephant is the largest land mammal. These species belong to the Proboscidea group. The characteristic of this group is their highly developed \'nose\' or proboscis- a muscular trunk that serves as their fifth limb. Of course, there are only two species of Proboscidea - the other specie being the Asian elephant. The largest Proboscidea species ever, which was about 30% larger than today\'s African elephant, was probably the Songhua River mammoth that lived until 10,000 years ago.',
-    photo: '/elephant.jpg'
+    photo: '/elephant.jpg',
+    category:'LARGEMAMMELS'
   },
   {
     id: 19,
     title: 'Fact File: Elephants',
     description: 'Elephants have huge feet and can weigh more than five tons. But their feet are big, soft and spongy and spread their weight out so well, that they barely leave footprints. The trunk of an elephant can hold up to two gallons of water.',
-    photo: '/elephant-facts.jpg'
+    photo: '/elephant-facts.jpg',
+    category:'LARGEMAMMELS'
   },
   {
     id: 20,
     title: 'Sirenia',
     description: 'Sirenians are a group of fully-aquatic marine mammals that live in swamps, rivers, estuaries, marine wetlands, and coastal marine waters. They are collectively known as \'sea-cows\' and only four species are living in two families, and these are the dugong and manatees. The West Indian manatee, which is the largest of all, includes two subspecies, the Florida Manatee and the Caribbean Manatee. Sea-cows are herbivores and graze on sea-grasses and related plants, though some will eat algae and floating monocots. Sirenians are solitary creatures, who live for more than 70 years and come together only to mate.',
-    photo: '/manatee.jpg'
+    photo: '/manatee.jpg',
+    category:'LARGEMAMMELS'
   }
 ];
